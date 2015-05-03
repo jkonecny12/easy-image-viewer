f1b06601032487dfdeba6be4c3a159e95f197372
Jiří Konečný <DragonLichcz@gmail.com>
2015-05-03 18:06:21 +0200

    Add users to settings

--- settings.cpp
+++ settings.cpp
@@ -14,7 +14,7 @@ Settings::~Settings()
 
 QString Settings::rootLocation() const
 {
-    return this->value(QStringLiteral("rootFolder")).toString();
+    return this->value(this->c_ROOT_FOLDER).toString();
 }
 
 void Settings::setRootLocation(QString path)
@@ -24,7 +24,43 @@ void Settings::setRootLocation(QString path)
 
     if(oldLocation != path)
     {
-        this->setValue(QStringLiteral("rootFolder"), QVariant(path));
+        this->setValue(this->c_ROOT_FOLDER, QVariant(path));
         emit this->rootLocationChanged(path);
     }
+}
+
+QStringList Settings::users() const
+{
+    return value(this->c_USERS).toStringList();
+}
+
+void Settings::addUser(QString user)
+{
+    QStringList list = this->users();
+    if(!list.contains(user))
+    {
+        setValue(this->c_USERS, QVariant(user));
+        emit this->usersChanged();
+    }
+}
+
+QStringList Settings::userSelectedList(QString user)
+{
+    QStringList groups = childGroups();
+    QStringList ret;
+
+    if(!groups.contains(user))
+    {
+        beginGroup(user);
+        ret = value(this->c_SELECTED_ITEMS).toStringList();
+        endGroup();
+    }
+    return ret;
+}
+
+void Settings::setUserSelectedList(QString user, QStringList userList)
+{
+    beginGroup(user);
+    setValue(this->c_SELECTED_ITEMS, QVariant(userList));
+    endGroup();
 }
--- settings.h
+++ settings.h
@@ -3,12 +3,14 @@
 
 #include <QObject>
 #include <QSettings>
+#include <QStringList>
 
 class Settings : public QSettings
 {
     Q_OBJECT
 
     Q_PROPERTY(QString rootLocation READ rootLocation WRITE setRootLocation NOTIFY rootLocationChanged)
+    Q_PROPERTY(QStringList users READ users NOTIFY usersChanged)
 public:
     explicit Settings(QObject *parent = 0);
     ~Settings();
@@ -16,8 +18,20 @@ public:
     QString rootLocation() const;
     void setRootLocation(QString path);
 
+    QStringList users() const;
+    void addUser(QString user);
+
+    QStringList userSelectedList(QString user);
+    void setUserSelectedList(QString user, QStringList userList);
+
 signals:
     void rootLocationChanged(QString);
+    void usersChanged();
+
+private:
+    QString c_ROOT_FOLDER = QStringLiteral("rootFolder");
+    QString c_USERS = QStringLiteral("users");
+    QString c_SELECTED_ITEMS = QStringLiteral("selectedItems");
 };
 
 #endif // SETTINGS_H
